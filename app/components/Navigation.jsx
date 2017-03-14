f423700310f109a17070e17420d92283b27b0113
Dustin K Greco <dgreco.dev@gmail.com>
2017-03-14 13:29:44 -0600

    Initial Commit

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,37 @@
+const React = require('react'),
+    {Link, IndexLink} = require('react-router');
+
+let Navigation;
+Navigation = () => {
+    "use strict";
+    return (
+        <div className="top-bar">
+            <div className="top-bar-left">
+                <ul className="menu">
+                    <li className="menu-text">
+                        React Timer
+                    </li>
+                    <li>
+                        <IndexLink to="/" activeClassName="active-link">
+                            Timer
+                        </IndexLink>
+                    </li>
+                    <li>
+                        <Link to="/" activeClassName="active-link">
+                            Countdown
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="top-bar-right">
+                <ul className="menu">
+                    <li className="menu-text">
+                        Created by <a href="https://www.linkedin.com/in/dkgreco/" target="_blank">Dustin Greco</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+};
+
+module.exports = Navigation;
